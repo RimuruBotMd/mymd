4f36b86e62257ddd3115608fc825dbdabcb80bfd
RimuruBotMd <108895247+RimuruBotMd@users.noreply.github.com>
2022-09-24 04:37:07 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895336282144]
-│ • Gopay  [0895336282144]
-│ • Dana  [0895336282144]
-│ • Saweria  [https://saweria.co/thesadboy01]
+│ • Smarft [088980870067]
+│ • Gopay  [088980870067]
+│ • Dana  [088980870067]
+│ • Saweria  [https://saweria.co/kannabotzz]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895336282144
+│ > Ingin donasi? Wa.me/6283866366437
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
